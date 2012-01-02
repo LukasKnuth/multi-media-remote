6e3bc82fbe794a72c183f25f0dca6de0851cfc17
Lukas Knuth <mr.luke.187@googlemail.com>
2012-01-03 00:10:12 +0100

    Improved the Log-component with a ScrollBar and auto-scroll.

--- Server/src/org/knuth/multimediaremote/server/view/elements/Log.java
+++ Server/src/org/knuth/multimediaremote/server/view/elements/Log.java
@@ -10,6 +10,8 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
 import java.awt.*;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 
 /**
  * @author Lukas Knuth
@@ -25,9 +27,12 @@ public class Log extends AppenderSkeleton{
     private static final StyledDocument doc;
 
     /** Used for the info-markup */
-    private final SimpleAttributeSet info_log;
+    private static final SimpleAttributeSet info_log;
     /** Used for the error-markup */
-    private final SimpleAttributeSet error_log;
+    private static final SimpleAttributeSet error_log;
+
+    /** ScrollPane used to scroll the log */
+    private JScrollPane scrollPane;
 
     /**
      * Initialize the components so Log4J and Swing use
@@ -37,6 +42,12 @@ public class Log extends AppenderSkeleton{
     static {
         log = new JTextPane();
         doc = log.getStyledDocument();
+        // Create the colors for the log:
+        info_log = new SimpleAttributeSet();
+        StyleConstants.setForeground(info_log, Color.green);
+        error_log = new SimpleAttributeSet();
+        StyleConstants.setForeground(error_log, Color.red);
+        StyleConstants.setBold(error_log, true);
     }
 
     /**
@@ -44,17 +55,6 @@ public class Log extends AppenderSkeleton{
      */
     public Log(){
         super();
-        // Do the basic setup for the Text-Pane
-        doBasicSetup();
-        // Create the SimpleAttributeSet styles
-        info_log = new SimpleAttributeSet();
-        StyleConstants.setForeground(info_log, Color.green);
-        error_log = new SimpleAttributeSet();
-        StyleConstants.setForeground(error_log, Color.red);
-        StyleConstants.setBold(error_log, true);
-
-        //append(null);
-        //append(null);
     }
 
     /**
@@ -68,6 +68,9 @@ public class Log extends AppenderSkeleton{
             // Append a new Info to the Log:
             try {
                 doc.insertString(doc.getLength(), message, info_log);
+                Rectangle rectangle = log.getVisibleRect();
+                rectangle.height += 10;
+                log.scrollRectToVisible(rectangle);
             } catch (BadLocationException e) {
                 e.printStackTrace();
             }
@@ -85,7 +88,25 @@ public class Log extends AppenderSkeleton{
      * Does the basic setup for the used {@code JTextPane}.
      */
     private void doBasicSetup(){
+        // Create the SimpleAttributeSet styles
         log.setEditable(false);
+        // Setup the ScrollPane:
+        scrollPane = new JScrollPane(log,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        // Enable auto-scroll:
+        scrollPane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener() {
+            @Override
+            public void adjustmentValueChanged(AdjustmentEvent e) {
+                e.getAdjustable().setValue(e.getAdjustable().getMaximum());
+            }
+        });
+        // No border:
+        scrollPane.setBorder(null);
+        // Get some space:
+        Dimension dimension = log.getPreferredSize();
+        dimension.height += 40;
+        scrollPane.setPreferredSize(dimension);
     }
 
     /**
@@ -93,8 +114,10 @@ public class Log extends AppenderSkeleton{
      *  on.
      * @return The {@code JTextPane}-instance.
      */
-    public JTextPane getView(){
-        return log;
+    public JComponent getView(){
+        // Check if already set up:
+        if (log.isEditable()) doBasicSetup();
+        return scrollPane;
     }
 
     /**
