c376c9e7d82ae1c0a42e40ee309a047ca75a761b
Lukas Knuth <mr.luke.187@googlemail.com>
2012-01-22 14:10:38 +0100

    Basic communication from the HTML-Page to the MMR-Server through AJAX works.

--- Server/src/org/knuth/multimediaremote/server/server/http/HttpMainServlet.java
+++ Server/src/org/knuth/multimediaremote/server/server/http/HttpMainServlet.java
@@ -4,8 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 /**
  * @author Lukas Knuth
@@ -15,6 +14,9 @@ import java.io.PrintWriter;
  */
 public class HttpMainServlet extends HttpServlet{
 
+    /** The cached index-file */
+    private String indexCache;
+
     /**
      * Gets called when the servlet is requested and returns the controls
      *  to send requests to the Server.
@@ -23,15 +25,41 @@ public class HttpMainServlet extends HttpServlet{
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         resp.setContentType("text/html");
-        PrintWriter out = null;
-        try {
-            out = new PrintWriter(resp.getWriter());
-            out.write("<h1>Hello World!</h1>");
-            out.write("<img src=\"test.png\" alt=\"shit\">");
+        try (PrintWriter out = new PrintWriter(resp.getWriter())) {
+            out.write(getIndexContents());
             out.flush();
-        } finally {
-            if (out != null) out.close();
+        } catch (IOException e){
+            // Problem reading from the file:
+            resp.sendError(404);
         }
+    }
+
+    /**
+     * Reads the "index.html"-file from the resource folder and caches it's
+     *  contents.
+     * @return the contents (from the file or the cache) of the index file.
+     * @throws IOException if the index-file could not be found or read.
+     */
+    private String getIndexContents() throws IOException {
+        // Check if already cached:
+        if (this.indexCache != null) return this.indexCache;
+        // Read contents from "index.html"-resource file:
+        InputStream is = this.getClass().getResourceAsStream("res/index.html");
+        if (is == null){
+            // If the "index.html"-file could not be found:
+            throw new FileNotFoundException("Couldn't find \"res/index.html\"-file");
+        }
+        // Read the file and cache the contents:
+        StringBuffer buffer = new StringBuffer();
+        try(BufferedReader in = new BufferedReader(new InputStreamReader(is))) {
+            // Copy contents:
+            String line;
+            while ( (line = in.readLine()) != null)
+                buffer.append(line);
+        }
+        // Write to cache:
+        this.indexCache = buffer.toString();
+        return indexCache;
     }
 
     /**
@@ -41,6 +69,11 @@ public class HttpMainServlet extends HttpServlet{
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException{
-
+        System.out.println("POST for action: "+req.getParameter("action"));
+        resp.setContentType("application/json");
+        try (PrintWriter out = new PrintWriter(resp.getWriter())){
+            out.write("{\"status\": \"success\"}");
+            out.flush();
+        }
     }
 }
---
+++ Server/src/org/knuth/multimediaremote/server/server/http/res/script.js
@@ -0,0 +1,48 @@
+/**
+* Assign methods to send the AJAX-requests for every button
+*  with the corresponding functionality.
+*/
+$(document).ready(function(){
+    // Pause/Play
+    $("#pause_play").click(function(){
+        // Send POST-request per AJAX.
+        sendAjaxPost("PAUSE_PLAY");
+    });
+    // Stop
+    $("#stop").click(function(){
+        sendAjaxPost("STOP");
+    })
+
+    $("html").ajaxError(function(event, request, settings){
+        console.log(request);
+        console.log(settings);
+    });
+});
+
+/**
+* Send an AJAX-request with the given action to the main
+*  servlet of the MMR-Server.
+* @param action_str the action to send to the MMR-Server
+*  as a String (same as for the MMR-protocol)
+*/
+function sendAjaxPost(action_str){
+    $.post("/", {action : action_str}, function(data){alert(data.status);}, "json")
+        .success(function(){ajaxPostSuccess();})
+        .error(function(){ajaxPostError();});
+}
+
+/**
+* Called if the request was successfully send. </p>
+* This method updates the "status"-span with a success-message.
+*/
+function ajaxPostSuccess(){
+    $("#status").text("Success!");
+}
+
+/**
+* Called if the request was not successfully send. </p>
+* Will update the "status"-span with a error-message.
+*/
+function ajaxPostError(){
+    $("#status").text("Error...");
+}
