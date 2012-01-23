ed6db063a03ade1b9fc42d15dafcb34acde0a5a1
Lukas Knuth <mr.luke.187@googlemail.com>
2012-01-23 21:43:32 +0100

    Improved the ErrorDialog-class to prompt the user with a problem for more usability.

--- Server/src/org/knuth/multimediaremote/server/view/elements/ErrorDialog.java
+++ Server/src/org/knuth/multimediaremote/server/view/elements/ErrorDialog.java
@@ -1,6 +1,9 @@
 package org.knuth.multimediaremote.server.view.elements;
 
+import org.knuth.multimediaremote.server.model.settings.Config;
+
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author Lukas Knuth
@@ -16,13 +19,19 @@ public class ErrorDialog {
     /** The title for the dialog */
     private final static String title = "This should not have happened...";
 
-    private final static String report_instructions = "<html><p>Please take " +
-            "a moment to report this problem, so we can fix it.</p>" +
+    /** The basic instructions to report a Bug/Issue */
+    private final static String report_instructions = "<p>Please take " +
+            "a moment to <b>report this problem</b>, so we can fix it.</p>" +
             "<p>You can report the problem by either:</p>" +
-            "<ul><li>opening a Ticket at <a href=\"http://github.com\">github</a></li>" +
-            "<li>or sending an E-Mail to <a href=\"mailto:mr.luke.187@googlemail.com\">Us</a>" +
+            "<ul><li>opening a <b>Ticket</b> at  " +
+                "<input type=\"text\" size=\"20\" value=\"http://github.com\" />" +
+            "<li>or sending an <b>E-Mail</b> to  " +
+                "<input type=\"text\" size=\"20\" value=\"mr.luke.187@googlemail.com\" />" +
             "</ul>" +
-            "<p>If you decide to do so, please add the Log-file from [dir] to your report!</p>" +
+            "<p>If you decide to do so, please <u>add the contents of your Log-file</u> to your report!</p>" +
+            "<p><b>Log-File:</b>  <input type=\"text\" size=\"40\" value=\"" +
+                Config.getBaseDir().toString()+"/logging.log" +
+            "\"/></p>" +
             "<p>Thanks for taking the time!</p></html>";
 
     /**
@@ -38,7 +47,7 @@ public class ErrorDialog {
      * Create a new ErrorDialog with the message from the
      *  supplied Throwable.</p>
      *  To show the dialog, use the {@code show()}-method.
-     * @param exception
+     * @param exception the exception to get the message from.
      */
     public ErrorDialog(Throwable exception){
         setUp(exception.getMessage());
@@ -50,18 +59,29 @@ public class ErrorDialog {
      * @param message the message to show on the dialog.
      */
     private void setUp(String message){
-        // TODO Handle Hyperlinks in JLabel with instructions
-        Object[] message_elements = new Object[]{
-                new JLabel(message),
-                new JLabel(report_instructions)
-        };
-        pane = new JOptionPane(message_elements,
+        pane = new JOptionPane(getTextPane(message),
                 JOptionPane.ERROR_MESSAGE,
                 JOptionPane.CLOSED_OPTION);
     }
 
     /**
-     * Show dialog.
+     * Set up the options to make the {@code JTextPane} look like a
+     *  {@code JLabel}.</p>
+     * This adds support for clickable Hyper-Links.
+     * @param message the error-message to show in this Label.
+     * @return the modified {@code JTextPane}.
+     */
+    private JTextPane getTextPane(String message){
+        final JTextPane text_pane = new JTextPane();
+        text_pane.setEditable(false);
+        text_pane.setBackground(new Color(238, 238, 238)); // Default BG-color of JLabel
+        text_pane.setContentType("text/html");
+        text_pane.setText("<html><p>"+message+"</p>"+report_instructions);
+        return text_pane;
+    }
+
+    /**
+     * Show the dialog.
      */
     public void show(){
         pane.createDialog(null, title).setVisible(true);
